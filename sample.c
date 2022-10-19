5301f01afbc72ac7cb6f105018ed276e28d83d0a
Achyut <aoogleak@gmail.com>
2022-10-19 11:43:31 +0545

    Sample file for description

---
+++ sample.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <math.h>
+#define PI 3.14
+
+int main()
+{
+//    int i;
+//    char str[100];
+//
+//    printf("Enter a number then your name ! \n");
+//    scanf("%d %s", &i, str);
+//
+//    printf(" Hi, %s Your Favorite number is %d", str, i);
+//
+//
+//    float point= 67.89;
+//    double xpoint= 55.7953790235790327509235;
+//    bool abc= false;
+//    printf("i think it is %d", abc);
+
+//    enum months {january, febuary, march, april, may, june, july, august};
+//    enum months favmonth= febuary;
+//    printf("favorite month is %d", favmonth);
+
+//    int num = abc;
+//    printf("%d", num);
+
+
+// int height, len, area;
+// printf("Enter height:");
+// scanf("%d", &height);
+// printf("Enter length:");
+// scanf("%d", &len);
+// area = height * len;
+// printf("Area is %d",area);
+
+    //printf("Hello world!\n");
+    
+    // int first, _2nd;
+    // int add,sub;
+    
+    // // getting user input
+    // printf("Enter 1st number: \n");
+    // scanf("%d", &first);
+    // printf("Enter 2nd number: \n");
+    // scanf("%d", &_2nd);
+    
+    // add= first + _2nd;
+    // sub = first - _2nd;
+    // printf("sum is %d \nsub is %d", add, sub);
+
+    // float Radius, area;
+    // printf("Enter radius: ");
+    // scanf("%f", &Radius);
+    // area= PI * Radius *Radius;
+    // printf("area is %.2f cm", area);
+
+    
+
+
+
+
+    return 0;
+}
+
+
+
